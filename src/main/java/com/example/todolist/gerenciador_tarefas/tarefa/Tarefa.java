7f35b127d56ecbf92376d1aee3ef4e6cd1a3801d
danielcostadev <danielcostamarketing@gmail.com>
2025-08-31 10:05:27 -0300

    Refactor: Substitui código boilerplate por anotações do Lombok

--- src/main/java/com/example/todolist/gerenciador_tarefas/tarefa/Tarefa.java
+++ src/main/java/com/example/todolist/gerenciador_tarefas/tarefa/Tarefa.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tarefa {
 
     @Id
@@ -18,63 +26,4 @@ public class Tarefa {
     private String prioridade;
     private String status;
 
-    public Tarefa() {
-    }
-
-    public Tarefa(Long id, String nome, String descricao, String categoria, String prioridade, String status) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.categoria = categoria;
-        this.prioridade = prioridade;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getPrioridade() {
-        return prioridade;
-    }
-
-    public void setPrioridade(String prioridade) {
-        this.prioridade = prioridade;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
