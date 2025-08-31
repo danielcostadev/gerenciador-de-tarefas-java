126c314c8ddb9fbfe4a0c53a9057050282e2f9be
danielcostadev <danielcostamarketing@gmail.com>
2025-08-31 09:41:38 -0300

    Feat: Adiciona entidade Tarefa com construtores, getters e setters

---
+++ src/main/java/com/example/todolist/gerenciador_tarefas/tarefa/Tarefa.java
@@ -0,0 +1,80 @@
+package com.example.todolist.gerenciador_tarefas.tarefa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private String prioridade;
+    private String status;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(Long id, String nome, String descricao, String categoria, String prioridade, String status) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.prioridade = prioridade;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
