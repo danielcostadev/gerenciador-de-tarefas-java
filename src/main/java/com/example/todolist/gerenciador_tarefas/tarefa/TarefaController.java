7c0de0812d965eae904879f714810e384428f0ac
@danielcostadev <danielcostamarketing@gmail.com>
2025-08-31 17:01:30 -0300

    Feat: Adiciona método para edição de tarefas

--- src/main/java/com/example/todolist/gerenciador_tarefas/tarefa/TarefaController.java
+++ src/main/java/com/example/todolist/gerenciador_tarefas/tarefa/TarefaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,5 +27,23 @@ public class TarefaController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(tarefaSalva);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefa> atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaAtributos){
+        Tarefa tarefaParaAtualizar = this.tarefaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tarefa não encontrada com o id: " + id));
+
+        tarefaParaAtualizar.setNome(tarefaAtributos.getNome());
+        tarefaParaAtualizar.setDescricao(tarefaAtributos.getDescricao());
+        tarefaParaAtualizar.setCategoria(tarefaAtributos.getCategoria());
+        tarefaParaAtualizar.setPrioridade(tarefaAtributos.getPrioridade());
+        tarefaParaAtualizar.setStatus(tarefaAtributos.getStatus());
+
+        Tarefa tarefaAtualizada = this.tarefaRepository.save(tarefaParaAtualizar);
+
+        return ResponseEntity.ok(tarefaAtualizada);
+
+    }
+
 
 }
